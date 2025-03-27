316c7bb59e4f9d1f4b3131cc0cb6195371d256c4
zeynepsanlli <zeynepsanli2002@gmail.com>
2025-03-27 10:39:03 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
-fetch('https://api.thecatapi.com/v1/images/search')
-  .then(response => response.json())
-  .then(data => {
-    const imageUrl = data[0].url;
-    const img = document.createElement('img');
-    img.src = imageUrl;
-    img.alt = "Random Cat";
-    img.style.width = "300px";
-    img.style.borderRadius = "10px";
-    document.body.appendChild(img);
-  })
-  .catch(error => console.log('Hata:', error));
+function getCat() {
+  fetch('https://api.thecatapi.com/v1/images/search')
+    .then(response => response.json())
+    .then(data => {
+      const imageUrl = data[0].url;
+      let img = document.getElementById('catImage');
+      if (!img) {
+        img = document.createElement('img');
+        img.id = 'catImage';
+        img.style.width = "300px";
+        img.style.borderRadius = "10px";
+        document.body.appendChild(img);
+      }
+      img.src = imageUrl;
+    })
+    .catch(error => console.log('Hata:', error));
+}
+
+window.onload = getCat;
